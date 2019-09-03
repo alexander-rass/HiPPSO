73397d034b18770017651a2f45c7b670293ea352
Alexander Raß <Alexander.Rass@fau.de>
2019-09-03 14:42:23 +0200

    remove *.h files from coveralls report

--- dev/display_test_coverage.sh
+++ dev/display_test_coverage.sh
@@ -2,9 +2,14 @@
 make clean
 rm -rf src/build
 make coveragetest
-cd src/build
-lcov -t "result" -o ex_text.info -c -d .
-genhtml -o res ex_text.info
+cd src/
+
+lcov -t "result" -o coverage_out.info -c -d ./build/;
+lcov --remove coverage_out.info "/usr/include/*" -o coverage_out2.info;
+lcov --remove coverage_out2.info "*.h" -o coverage_out3.info;
+sed "s/SF:.*\/HiPPSO\/src\//SF:/" coverage_out3.info > coverage_out4.info;
+lcov --list coverage_out4.info;
+genhtml -o res coverage_out4.info
 
 firefox res/index.html &
-cd ../..
+cd ..
