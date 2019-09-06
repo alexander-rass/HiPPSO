bf58a127e55818120b9e9679386f5bd8fc90b9b6
Alexander Raß <Alexander.Rass@fau.de>
2019-09-06 11:39:41 +0200

    add new test into test suite

--- src/testing/test_data_sets/check_commandline_arguments/doTest.sh
+++ src/testing/test_data_sets/check_commandline_arguments/doTest.sh
@@ -2,6 +2,15 @@
 
 source ../compareFolders.sh
 
+REFFOLDER=reference_data
+TESTBASEFOLDER=$(pwd)
+
+if [ -d $REFFOLDER ] ; then
+    echo "Start test.";
+else
+    echo "Missing REFFOLDER data.";
+    exit 1;
+fi
 
 setsid ./createDataRestartAll.sh testfolder
 
@@ -9,9 +18,16 @@ EXITCODE=0
 for TESTID in $(seq 1 3); do
 	checkFoldersMatching "testfolder/stage$TESTID" "reference_data/stage$TESTID"  || EXITCODE=1
 	if [ $EXITCODE -eq 1 ]; then
+		cd $TESTBASEFOLDER
 		exit 1;
 	fi
 done
 rm -r testfolder
+
+if [ $EXITCODE -eq 0 ] ; then
+    echo "Test succeeded."
+else
+    echo "TEST FAILED!!"
+fi
 exit $EXITCODE
 
